0cb8b0505d6d0180c16e007f76c007fd5bf6e781
jmuldvp <jmul.dvp@gmail.com>
2018-09-20 21:25:01 -0600

    CP 4 assignment submission 2

--- cp4/bigo-factorial.rb
+++ cp4/bigo-factorial.rb
@@ -1,13 +1,15 @@
-def worst_bigo(num)
-  n = 1
-  (n..num).each do |i|
-    n = n * i
-    puts i.to_s + ', ' + n.to_s
+def worst_o_of_n(num)
+  i = 1
+  while i < num + 1
+    if i == num
+      puts "found #{num}"
+    end
+    i += 1
   end
   puts "--- End of section where number = #{num} ---"
 end
 
-puts "Big-O notation of worst case iteration values O(n!)\n\n"
+puts "Big-O notation of worst case iteration values O(n)\n\n"
 worst_bigo(4)
 worst_bigo(8)
 worst_bigo(10)
