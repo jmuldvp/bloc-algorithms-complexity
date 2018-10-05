54084dc7965b361562a16a124ead9c165ab3600b
jmuldvp <jmul.dvp@gmail.com>
2018-10-04 21:31:03 -0600

    CP 6 assignment completed

--- cp6/improving_complexity_version_three.rb
+++ cp6/improving_complexity_version_three.rb
@@ -7,33 +7,8 @@ class ArrSort
     @arr = Hash.new
   end
 
-  
-end
-
-def improving_complexity_version_three(*arrays)
-  combined_array = []
-  arrays.each do |array|
-    array.each do |value|
-      combined_array << value
-    end
+  def combine_sort(*arrays)
+    h = arrays.flatten.sort
+    @arr = Hash[(1..h.size).zip h]
   end
-
-  sorted_array = [combined_array.delete_at(combined_array.length-1)]
-
-  for val in combined_array
-    i = 0
-    while i < sorted_array.length
-      if val <= sorted_array[i]
-        sorted_array.insert(i, val)
-        break
-      elsif i == sorted_array.length - 1
-        sorted_array << val
-        break
-      end
-      i+=1
-    end
-  end
-
-  # Return the sorted array
-  sorted_array
 end
